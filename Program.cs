34a55f54ff2cf2bae7da724a25f9d7c25baf6bb8
ekatsiashvili <katsiashvili.elena@gmail.com>
2024-11-17 21:05:56 +0000

    Revert "commit 1"
    This reverts commit d585d2ee9f9b1142f2ef3a0129acd0ed37071ab3.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace MyConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("Feature branch 1 - development 2");
 
         }
     }
